99ee686dab409d246ccf6617a9fd3839a0dd15ac
sprite-toolkit <sprite-toolkit@local>
2026-06-24 17:35:21 +0800

    feat: scaffold sprite-toolkit vue3+vite project

---
+++ sprite-toolkit/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Sprite Toolkit</h1>
+  </div>
+</template>
---
+++ sprite-toolkit/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+export default defineConfig({
+  plugins: [vue(), viteSingleFile()],
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: 100000000,
+    cssCodeSplit: false,
+  },
+})
